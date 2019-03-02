5b4b7bfdc003527f7d5ef26322cb8fd07594e8db
Nadica08 <nadiabc3@gmail.com>
2019-03-01 22:58:40 -0800

    made changes. created a base html and a refactor py file

--- build.py
+++ build.py
@@ -62,3 +62,5 @@ for page in pages:
 page_title = page['title']
 print(page_title)    
 
+
+
---
+++ refactor.py
@@ -0,0 +1,14 @@
+class page(pages){
+    main()
+    };
+  
+page(filename) {}; 0;
+
+page(output) {};
+
+page(title) {};
+
+print(pages)
+main()
+    
+    
